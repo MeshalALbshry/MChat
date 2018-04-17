0039585b76ebe652d7f523e37c817c6608f8f934
MESHAL <meshal.albshry@gmail.com>
2018-04-17 10:03:27 +0300

    UserContactDeleteAndAddToGather

--- Mohdthat/Controllers/AddContactController.cs
+++ Mohdthat/Controllers/AddContactController.cs
@@ -41,26 +41,43 @@ namespace Mohdthat.Controllers
         {
             var currnetUser = User.Identity.Name;
             var user_added = db.Users.SingleOrDefault(u => u.Id == id);
+            var cuser_added = db.Users.SingleOrDefault(u => u.UserName == currnetUser);
+
             if (user_added == null)
                 return HttpNotFound();
 
             db.UserContacts.Add(new User_Contacts
             {
                 CurrnetUser = currnetUser,
-                User = user_added,
                 CreatedAt = DateTime.Now,
+                User = user_added,
                 UserSelected = user_added.UserName
             });
+
+            //To add in another user
+            db.UserContacts.Add(new User_Contacts
+            {
+                CurrnetUser = user_added.UserName,
+                CreatedAt = DateTime.Now,
+                User = cuser_added,
+                UserSelected = currnetUser
+            });
             db.SaveChanges();
+            
             return RedirectToAction("Index","AddContact");
         }
         public ActionResult Delete(string id)
         {
+            var currnetUser = User.Identity.Name;
+            var cuser_added = db.Users.SingleOrDefault(u => u.UserName == currnetUser);
+
             var userCon = db.UserContacts.FirstOrDefault(u => u.UserID == id);
+            var cUserCon = db.UserContacts.FirstOrDefault(u => u.UserSelected == currnetUser);
             if (id == null)
                 return HttpNotFound();
 
             db.UserContacts.Remove(userCon);
+            db.UserContacts.Remove(cUserCon);
             db.SaveChanges();
             return RedirectToAction("Index", "AddContact");
         }
